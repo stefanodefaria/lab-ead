59b8341bc016a02c19c469f4b5cc73f0b4424977
stefano <stefanodefaria@gmail.com>
2015-10-24 00:58:28 -0200

    Adicionado modulo de testes para medir tempo de resposta do Arduino

--- test.js
+++ test.js
@@ -1,3 +1,25 @@
-var utils = require('./utils');
+var logger = require('./testLogger').createLog('arduinoResponse'),
+    serialDevice = require('./serialDevice');
 
-utils.catchErr(new Error("blablabla"));
+var arduinoID = 'usb-Arduino__www.arduino.cc__0043_952333532313512101D1-if00',
+    callsCount = 0, totalCalls = 100,
+    arduino;
+
+function callArduino(){
+    if(callsCount < totalCalls){
+        callsCount++;
+        var start = Date.now();
+
+        arduino.deviceIsAvailable(function(){
+            logger.log((Date.now() - start).toString() + '\n'); // Coleta tempo de resposta
+            callArduino(arduino);
+        });
+    }
+    else{ console.log("Teste finalizado"); }
+}
+
+serialDevice.startDevice(arduinoID, {}, function(_, device){
+    console.log("Arduino inicializado. Iniciando teste...");
+    arduino = device;
+    arduino.deviceIsAvailable(callArduino);
+});
